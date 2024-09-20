9966e430bdc62ac7b3c4c0de779e1ed647f3c8af
Gustavo Esteves <gustavoesteves@directon.com.br>
2024-09-20 12:14:44 -0300

    feat: Criação do componente right-container

---
+++ src/app/components/right-container/right-container.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-right-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './right-container.component.html',
+  styleUrl: './right-container.component.scss'
+})
+export class RightContainerComponent {
+
+}
