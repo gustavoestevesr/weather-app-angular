48bfe93e47313491f619feac2b9074143a932152
Gustavo Esteves <gustavoesteves@directon.com.br>
2024-09-20 12:14:18 -0300

    feat: Criação do container principal e sub-containeres com CSS no padrão BEM

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LeftContainerComponent } from './components/left-container/left-container.component';
+import { RightContainerComponent } from './components/right-container/right-container.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [LeftContainerComponent, RightContainerComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'weather-app-angular';
+
 }
